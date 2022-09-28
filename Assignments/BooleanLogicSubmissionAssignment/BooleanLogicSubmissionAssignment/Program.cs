81150ebd5c943d3ea39fa8142f61714e2b5895c4
Itz-Djin <Christdjin.B@gmail.com>
2022-09-27 20:56:48 -0600

    Boolean Assignment

--- Assignments/BooleanLogicSubmissionAssignment/BooleanLogicSubmissionAssignment/Program.cs
+++ Assignments/BooleanLogicSubmissionAssignment/BooleanLogicSubmissionAssignment/Program.cs
@@ -10,6 +10,20 @@ namespace BooleanLogicSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Insurance Application");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\" ");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have? Please use integer number.");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = age > 15 && DUI == false && tickets < 3;
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
         }
     }
 }
