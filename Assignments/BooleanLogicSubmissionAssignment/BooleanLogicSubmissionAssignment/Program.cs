2cc3787076b12f721888212ddaf38519a88e003f
Itz-Djin <Christdjin.B@gmail.com>
2022-09-27 21:24:13 -0600

    Conditional Statements challenge

--- Assignments/BooleanLogicSubmissionAssignment/BooleanLogicSubmissionAssignment/Program.cs
+++ Assignments/BooleanLogicSubmissionAssignment/BooleanLogicSubmissionAssignment/Program.cs
@@ -13,13 +13,18 @@ namespace BooleanLogicSubmissionAssignment
             Console.WriteLine("Insurance Application");
             Console.ReadLine();
 
+            // qualifying questions
             Console.WriteLine("What is your age?");
+            //Converts string value given to an int value
             int age = Convert.ToInt32(Console.ReadLine());
+            //Converts string value given to a boolean value
             Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\" ");
             bool DUI = Convert.ToBoolean(Console.ReadLine());
+            //Converts string value given to a int value
             Console.WriteLine("How many speeding tickets do you have? Please use integer number.");
             int tickets = Convert.ToInt32(Console.ReadLine());
 
+            //Determines if person is qualified, they must meet all 3 given criteria
             bool qualified = age > 15 && DUI == false && tickets < 3;
             Console.WriteLine("Qualified?");
             Console.WriteLine(qualified);
---
+++ Assignments/ConditionalStatementsChallenge/ConditionalStatementsChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatementsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+            if(favNum > 5)
+            {
+                Console.WriteLine("Your favorite number is greater than 5");
+            }
+            else if(favNum < 5)
+            {
+                Console.WriteLine("Your favorite number is less than 5");
+            }
+            else if(favNum == 5)
+            {
+                Console.WriteLine("5 is a great number");
+            }
+            else
+            {
+                Console.WriteLine("Please provide a numerical value not a string value");
+            }
+            Console.ReadLine();
+        }
+    }
+}
