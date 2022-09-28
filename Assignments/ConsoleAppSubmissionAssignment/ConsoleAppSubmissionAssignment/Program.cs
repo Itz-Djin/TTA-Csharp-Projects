379cf7a8d46678471584ffaba6b0ded8b0d21ec0
Itz-Djin <Christdjin.B@gmail.com>
2022-09-28 15:30:59 -0600

    Console App Submission Assignment

---
+++ Assignments/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give any number.");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 500);
+            Console.ReadLine();
+        }
+    }
+}
