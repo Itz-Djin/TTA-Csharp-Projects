25b9b0985ab773fb2a7a43794ddc9a86b33b788e
Itz-Djin <Christdjin.B@gmail.com>
2022-09-28 15:00:14 -0600

    Update Branching Assignment

--- Assignments/BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
+++ Assignments/BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
@@ -20,33 +20,37 @@ namespace BranchingSubmissionAssignment
             if (pkgWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-                Console.ReadLine();
-                return;
+                //Console.ReadLine();
+                //return;
             }
-
-            //Converts package width to int data type
-            Console.WriteLine("Please enter the package width?");
-            int pkgWidth = Convert.ToInt32(Console.ReadLine());
-
-            //Converts package height to int data type
-            Console.WriteLine("Please enter the package height?");
-            int pkgHeight = Convert.ToInt32(Console.ReadLine());
-
-            //Converts package lengt to int data type
-            Console.WriteLine("Please enter the package length?");
-            int pkgLength = Convert.ToInt32(Console.ReadLine());
-
-            //multiplies package width, height and length, if product of that is less than 50 proceed, if not end program
-            if (pkgWidth * pkgHeight * pkgLength > 50)
+            else
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-                Console.ReadLine();
-                return;
-            }
+                //Converts package width to int data type
+                Console.WriteLine("Please enter the package width?");
+                int pkgWidth = Convert.ToInt32(Console.ReadLine());
 
-            //Creates the quote if package meets all above requirements
-            int quote = (((pkgWidth * pkgHeight * pkgLength) * pkgWeight) / 100);
-            Console.WriteLine("$" + Convert.ToString(quote) + ".00");
+                //Converts package height to int data type
+                Console.WriteLine("Please enter the package height?");
+                int pkgHeight = Convert.ToInt32(Console.ReadLine());
+
+                //Converts package lengt to int data type
+                Console.WriteLine("Please enter the package length?");
+                int pkgLength = Convert.ToInt32(Console.ReadLine());
+
+                //multiplies package width, height and length, if product of that is less than 50 proceed, if not end program
+                if (pkgWidth + pkgHeight + pkgLength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    //Console.ReadLine();
+                    //return;
+                }
+                else
+                {
+                    //Creates the quote if package meets all above requirements
+                    int quote = (((pkgWidth * pkgHeight * pkgLength) * pkgWeight) / 100);
+                    Console.WriteLine("$" + Convert.ToString(quote) + ".00");
+                }
+            }
 
             Console.ReadLine();
         }
