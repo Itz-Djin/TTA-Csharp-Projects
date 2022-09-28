9621e11e6c06211c78ab6a3b67df3492682d1074
Itz-Djin <Christdjin.B@gmail.com>
2022-09-28 15:47:43 -0600

    Updated

--- Assignments/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
+++ Assignments/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
@@ -11,13 +11,23 @@ namespace ConsoleAppSubmissionAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Please give any number.");
+            //Converts user input to int data type
             int i = Convert.ToInt32(Console.ReadLine());
 
+            //while loop where if user input is greater than 1, decrement by 1 till number is reached
+            while (i > 1)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            Console.ReadLine();
+            //Tries loop once even if false
             do
             {
                 Console.WriteLine(i);
                 i++;
             }
+            //if condition is false than allows increment to occur, if user input is less than 500 increment by 1 till 500
             while (i < 500);
             Console.ReadLine();
         }
---
+++ Assignments/StringFuncctions/StringFuncctions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFuncctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jin";
+            //string quote = "The man said, \"Hello\", Jin. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C:\Users\Jin";
+
+            //bool trueOrFalse = name.Contains("s");
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jin");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
