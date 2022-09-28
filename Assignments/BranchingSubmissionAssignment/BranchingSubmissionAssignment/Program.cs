dacc98b5db4e73ca460777f961da0bb3997f0887
Itz-Djin <Christdjin.B@gmail.com>
2022-09-27 22:20:16 -0600

    update

--- Assignments/BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
+++ Assignments/BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
@@ -14,7 +14,9 @@ namespace BranchingSubmissionAssignment
             Console.ReadLine();
 
             Console.WriteLine("Please enter the package weight?");
+            //Converts user input value to int data type
             int pkgWeight = Convert.ToInt32(Console.ReadLine());
+            //if package weight is greater than 50 end program (return), otherwise continue 
             if (pkgWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
@@ -22,15 +24,19 @@ namespace BranchingSubmissionAssignment
                 return;
             }
 
+            //Converts package width to int data type
             Console.WriteLine("Please enter the package width?");
             int pkgWidth = Convert.ToInt32(Console.ReadLine());
 
+            //Converts package height to int data type
             Console.WriteLine("Please enter the package height?");
             int pkgHeight = Convert.ToInt32(Console.ReadLine());
 
+            //Converts package lengt to int data type
             Console.WriteLine("Please enter the package length?");
             int pkgLength = Convert.ToInt32(Console.ReadLine());
 
+            //multiplies package width, height and length, if product of that is less than 50 proceed, if not end program
             if (pkgWidth * pkgHeight * pkgLength > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
@@ -38,6 +44,7 @@ namespace BranchingSubmissionAssignment
                 return;
             }
 
+            //Creates the quote if package meets all above requirements
             int quote = (((pkgWidth * pkgHeight * pkgLength) * pkgWeight) / 100);
             Console.WriteLine("$" + Convert.ToString(quote) + ".00");
 
