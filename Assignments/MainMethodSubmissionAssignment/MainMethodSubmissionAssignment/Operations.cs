cc69387919684aa21413f6e1fd89e28701c67e09
Itz-Djin <Christdjin.B@gmail.com>
2022-09-30 14:27:01 -0600

    update

--- Assignments/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Operations.cs
+++ Assignments/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Operations.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace MainMethodSubmissionAssignment
 {
-    class Operations
+    public class Operations
     {
-        public static int addition(int x)
+        //Method Additon for int values
+        public int Addition(int x)
         {
             return 10 + x;
+        }
+
+        //Method Addition for decimal values, returns result as int 
+        public int Addition(decimal x)
+        {
+            //return 1.5m * x;
+            return Convert.ToInt32(1.5m * x);
+        }
+
+        //Method Addition for string values, returns result as int
+        public int Addition(string x)
+        {
+            int y = Convert.ToInt32(x);
+            //return 2 * Convert.ToInt32(x);
+            return 2 * y;
         }
     }
 }
--- Assignments/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
+++ Assignments/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -10,10 +10,25 @@ namespace MainMethodSubmissionAssignment
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Please Provide a number");
             int number = 5;
-            int addition = Operations.addition(number);
-            Console.WriteLine(number);
+            decimal num1 = 5.5m;
+            string str1 = "10";
+
+            //instantiates our class Operations and assigns it to identifier operations (lower vs upper case "o")
+            Operations operations = new Operations();
+            //call Addition method of the operations class and number is passed in as the argument
+            Console.WriteLine(operations.Addition(number));
+
+            //does the second Additon method (decimal) of the operations class,
+            //converts result to int and assigns to intNum1
+            //int intNum1 = Convert.ToInt32(operations.Addition(num1));
+            //Console.WriteLine(intNum1);
+            Console.WriteLine(operations.Addition(num1));
+
+            Console.WriteLine(operations.Addition(str1));
+
+
+
             Console.ReadLine();
         }
     }
