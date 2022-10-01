4aafbd2aa12c5cbdf14cfc5c436f906068137e2e
Itz-Djin <Christdjin.B@gmail.com>
2022-09-30 23:05:24 -0600

    method and objects submisssion assignment

---
+++ Assignments/MethodsAndObjectsSubmissionAssignment/MethodsAndObjectsSubmissionAssignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsSubmissionAssignment
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: {0}", FullName);
+        }
+    }
+}
