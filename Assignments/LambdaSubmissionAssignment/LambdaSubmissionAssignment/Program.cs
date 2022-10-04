cef1b8f6fe7b5d817ef7c8d38b3e8e3faa4f7e0d
Itz-Djin <Christdjin.B@gmail.com>
2022-10-04 16:47:04 -0600

    Lambda Submission ASsignment

--- Assignments/LambdaSubmissionAssignment/LambdaSubmissionAssignment/Program.cs
+++ Assignments/LambdaSubmissionAssignment/LambdaSubmissionAssignment/Program.cs
@@ -10,34 +10,58 @@ namespace LambdaSubmissionAssignment
     {
         static void Main(string[] args)
         {
-            List<string> employees = new List<string>()
+            //Employee emp = new Employee();
+
+            List<Employee> employees = new List<Employee>()
             {
-                "Joe",
-                "James",
-                "Adam",
-                "Leeya",
-                "Joe",
-                "Jin",
-                "Serena",
-                "Rich",
-                "Henry",
-                "Silvester"
+                new Employee() { Id = 1, FirstName = "Joe", LastName = "Kron" },
+                new Employee() { Id = 2, FirstName = "Joe", LastName = "Smith" },
+                new Employee() { Id = 3, FirstName = "Katt", LastName = "Brady" },
+                new Employee() { Id = 4, FirstName = "Vai", LastName = "Guy" },
+                new Employee() { Id = 5, FirstName = "Jin", LastName = "Kron" },
+                new Employee() { Id = 6, FirstName = "Sally", LastName = "Kron" },
+                new Employee() { Id = 7, FirstName = "Adam", LastName = "Kron" },
+                new Employee() { Id = 8, FirstName = "Spencer", LastName = "Kron" },
+                new Employee() { Id = 9, FirstName = "Scott", LastName = "Kron" },
+                new Employee() { Id = 10, FirstName = "Jay", LastName = "Kron" }
             };
 
-            //List<string> listOfJoes = new List<string>();
+            List<Employee> listOfJoes = new List<Employee>();
 
-            //foreach (string employee in employees)
-            //{
-            //    if(employee == "Joe")
-            //    {
-            //        listOfJoes.Add(employee);
-            //    }
+            //creates employee variable of employees
+            foreach (Employee employee in employees)
+            {
+                //if "Joe" is found in employee
+                if(employee.FirstName == "Joe")
+                {
+                    //add "Joe" to listOfJoes
+                    listOfJoes.Add(employee);
+                }
 
-            //}
-            //Console.WriteLine(listOfJoes);
+            }
+            foreach (Employee joe in listOfJoes)
+            {
+                Console.WriteLine(joe.FirstName + " " + joe.LastName);
+            }
+            Console.ReadLine();
 
-            var listOfJoes2 = employees.Where(n => n == "Joe");
-            Console.WriteLine(listOfJoes2);
+            //goes through list of employees and adds to listOfJoes2,
+            //if the name "Joe" is found in employees
+            List<Employee> listOfJoes2 = employees.Where(n => n.FirstName == "Joe").ToList();
+            foreach (Employee joe2 in listOfJoes2)
+            {
+                Console.WriteLine(joe2.FirstName + " " + joe2.LastName);
+            }
+            Console.ReadLine();
+
+            //iterates through empId, finds Id's over 5
+            //adds those over 5 to idOver5
+            List<Employee> idOver5 = employees.Where(x => x.Id > 5).ToList();
+            foreach (Employee id in idOver5)
+            {
+                Console.WriteLine(id.Id + " " + id.FirstName);
+            }
+
             Console.ReadLine();
         }
     }
