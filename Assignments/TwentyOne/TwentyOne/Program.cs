ce23613c61ce7a04090e6414b6a68f0b91c71a07
Itz-Djin <Christdjin.B@gmail.com>
2022-10-06 00:04:57 -0600

    update

--- Assignments/TwentyOne/TwentyOne/Program.cs
+++ Assignments/TwentyOne/TwentyOne/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+using Casino;
 
 namespace TwentyOne
 {
@@ -11,7 +7,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
