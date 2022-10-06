207ab5af5ede934e9844785b602b30a853819876
Itz-Djin <Christdjin.B@gmail.com>
2022-10-06 13:30:23 -0600

    DateTime Submission Assignment

--- Assignments/DateTimeSubmissionAssignment/DateTimeSubmissionAssignment/Program.cs
+++ Assignments/DateTimeSubmissionAssignment/DateTimeSubmissionAssignment/Program.cs
@@ -10,12 +10,17 @@ namespace DateTimeSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            //instantiates new DateTime object
             DateTime now = DateTime.Now;
 
+            //shows only time of DateTime
             Console.WriteLine(now.ToString("hh:mm tt"));
             Console.WriteLine("Please enter a number.");
+            //Converts user input to data type of double
             Double x = Convert.ToDouble(Console.ReadLine());
+            //adds user input to current time, instantiating it to later
             DateTime later = DateTime.Now.AddHours(x);
+            //writes only the time of the DateTime added from user input
             Console.WriteLine(later.ToString("hh:mm tt"));
             Console.ReadLine();
         }
