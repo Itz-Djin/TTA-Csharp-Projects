7750780e624f046c0943b0a670c24a714555a481
Itz-Djin <Christdjin.B@gmail.com>
2022-10-06 18:39:16 -0600

    con assignment

--- Assignments/ConstructorSubmissionAssignment/ConstructorSubmissionAssignment/Program.cs
+++ Assignments/ConstructorSubmissionAssignment/ConstructorSubmissionAssignment/Program.cs
@@ -10,6 +10,28 @@ namespace ConstructorSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            //creating a variable that cannot be changed
+            const string slcVinylCo = "SLCVinylCo";
+            //using var to create a variable, var implicitly detects what the value is
+            var x = 369;
+        }
+        //creating new class
+        public class People
+        {
+            public string name;
+            public int age;
+
+            //chaining constructors 
+            //inherits from other People constructor but defaults age to 18
+           public People(string name) : this(name, 18)
+            {
+
+            } 
+           public People(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
         }
     }
 }
