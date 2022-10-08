5a3298b926d11a4a6d69f2eebc6b5a730fe885a5
Itz-Djin <Christdjin.B@gmail.com>
2022-10-08 14:11:18 -0600

    form update

--- Assignments/NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ Assignments/NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +13,44 @@ namespace NewsletterAppMVC.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=Newsletter;Integrated
+                                            Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                                            ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+
+                //by wrapping a outside connection with a using statement, it helps prevents memory leaks,
+                //diff than using statements at the top
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+                return View("Success");
+            }
         }
 
         public ActionResult About()
